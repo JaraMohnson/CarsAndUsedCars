b86db1582955881b7e431ebbc6f22580d6d7baca
JaraMohnson <jaramohnson@gmail.com>
2022-07-07 15:27:49 -0400

    First commit - two classes created

---
+++ Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsAndUsedCars
+{
+    internal class Car
+    {
+        //properties
+        public string Make { get; set; } 
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        //default constructor 
+        public Car()
+        {
+            Make = "";
+            Model = "";
+            Year = 0;
+            Price = 0;
+        }
+
+        // 4 argument constructor 
+        public Car(string _make, string _model, int _year, decimal _price)
+        {
+            Make = _make;
+            Model = _model;
+            Year = _year;
+            Price = _price;
+        }
+
+        // methods
+        public string ToString(Car c)
+        {
+            return $"{Year} {Make} {Model} is listed at ${Price}";
+        }
+
+    }
+}
---
+++ UsedCar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsAndUsedCars
+{
+    internal class UsedCar : Car
+    {
+        //property 
+        public double Mileage { get; set; }
+
+
+        // Constructor 
+        public UsedCar(string _make, string _model, int _year, decimal _price, double _mileage): 
+            base( _make,  _model,  _year,  _price)
+        {
+            Mileage = _mileage;
+        }
+
+
+        // tentative method.... 
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+
+
+
+
+    }
+}
